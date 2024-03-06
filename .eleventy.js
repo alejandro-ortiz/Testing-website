716b3793faa54ba30cc234a2d627f10bfc73f96f
Alex Ortiz <alex@ortizaggies.com>
2024-03-06 13:23:21 -0600

    test to see if css works because strange things are going on right now

---
+++ .eleventy.js
@@ -0,0 +1,29 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addLiquidFilter("relative_url", function(url) {
+    // Prepend the site's base URL to the given URL
+    return "https://alejandro-ortiz.github.io/testing-website" + url;
+  });
+  // Set custom site data
+  eleventyConfig.addPassthroughCopy("images");
+
+  // return {
+  //   dir: {
+  //     input: ".",
+  //     output: "_site",
+  //     includes: "_includes",
+  //     data: "_data"
+  //   },
+  //   passthroughFileCopy: true,
+  //   templateFormats: ["html", "md", "njk"],
+  //   htmlTemplateEngine: "njk",
+  //   markdownTemplateEngine: "njk",
+  //   dataTemplateEngine: "njk",
+  //   // Custom site data
+  //   templateFormats: ["html", "md", "njk", "liquid"],
+  //   htmlTemplateEngine: "liquid",
+  //   markdownTemplateEngine: "liquid",
+  //   passthroughFileCopy: true,
+  //   // Add 'README.md' to Eleventy's ignore list
+  //   ignoredFiles: ["README.md"]
+  // };
+};
